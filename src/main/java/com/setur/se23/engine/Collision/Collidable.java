166445717f10a70f1b9d1a5fdb2199dfe5ab625a
RagnarKD <2022.207@student.setur.fo>
2023-11-30 10:34:18 +0000

    shortened Collidable interface

--- src/main/java/com/setur/se23/engine/Collision/Collidable.java
+++ src/main/java/com/setur/se23/engine/Collision/Collidable.java
@@ -1,8 +1,8 @@
 package com.setur.se23.engine.Collision;
 
+import com.setur.se23.engine.core.Entity;
+
 public interface Collidable {
-    
-    public void collide();
-    public void collisionEvent();
+    public void collisionEvent(Entity collisionEntity);
 }
 
