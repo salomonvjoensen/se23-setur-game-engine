fa641153edf5d909f222d58ef331511a1dfa3178
iggysniggy <ingmarbm@gmail.com>
2023-12-05 17:12:56 +0000

    Add Audio component and dependencies

---
+++ src/main/java/com/setur/se23/engine/audio/Audio.java
@@ -0,0 +1,13 @@
+package com.setur.se23.engine.audio;
+
+import javafx.scene.media.AudioClip;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.Group;
+import javafx.stage.Stage;
+
+public class Audio {
+    MediaPlayer mediaPlayer;
+    Media media;
+    AudioClip audioClip;
+}
