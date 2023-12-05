e210635acde0024c4603f7e3037f6c19b5f9bea9
RagnarKD <2022.207@student.setur.fo>
2023-12-05 11:12:09 +0000

    Created basic scene manager.

---
+++ src/main/java/com/setur/se23/SceneManager/SceneManager.java
@@ -0,0 +1,43 @@
+package com.setur.se23.SceneManager;
+
+import java.util.Scanner;
+
+import com.setur.se23.FlappyBird.FlappyBird;
+import com.setur.se23.GameTemplate.GameTemplate;
+
+public class SceneManager {
+
+    private static String scene = "1";
+    
+    public static void manage() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("1: Flappy Bird");
+        System.out.println("2: Second Game");
+        System.out.println("3: GameTemplate");
+        System.out.println("");
+        System.out.println("Scene number: ");
+
+        scene = scanner.nextLine();
+
+        scanner.close();
+    }
+
+    public static void load() {
+
+        if (scene.equals("1")) {
+            FlappyBird flappyBird = new FlappyBird();
+            flappyBird.gameLoop.start();
+        }
+
+        if (scene.equals("2")) {
+            
+        }
+
+        if (scene.equals("3")) {
+            GameTemplate gameTemplate = new GameTemplate();
+            gameTemplate.gameLoop.start();
+        }
+    }
+}
