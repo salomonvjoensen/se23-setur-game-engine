61f3de43e898f2ccf8e7f5a678aa8084e2619e0c
Salomon Vágadal Joensen <2022.210@student.setur.fo>
2023-12-07 14:21:22 +0000

    Adds movement methods & position.

--- src/main/java/com/setur/se23/snake/Snake_Objects/SnakeHead.java
+++ src/main/java/com/setur/se23/snake/Snake_Objects/SnakeHead.java
@@ -8,14 +8,57 @@ import com.setur.se23.engine.render.common.Texture2D;
 
 public class SnakeHead extends Entity {
 
-public SnakeHead(double xPos, double yPos, double angle) {
-        super(new Material(
-                    new Texture2D(Core.getSprite("snake-head.png"), 220, 170),
-                    new MaterialColour(1.0f, 0.0f, 0.0f, 1.0f)), 
-              xPos, 
-              yPos, 
-              angle,
-              0.1, 
-              0.1);
-    }  
+    private int directionX;
+    private int directionY;
+
+    public SnakeHead(double xPos, double yPos, double angle) {
+            super(new Material(
+                        new Texture2D(Core.getSprite("snake-head.png"), 160, 160),
+                        new MaterialColour(1.0f, 0.0f, 0.0f, 1.0f)), 
+                xPos, 
+                yPos, 
+                angle,
+                0.1, 
+                0.1);
+        }
+
+    public void setPosition(int i, int j) {
+
+    }
+
+    public void movingUp(boolean moving) {
+        if (moving) {
+            directionX = 0;
+            directionY = -1; // Upwards movement decreases Y-coordinate
+        }
+    }
+
+    public void movingLeft(boolean moving) {
+        if (moving) {
+            directionX = -1; // Leftwards movement decreases X-coordinate
+            directionY = 0;
+        }
+    }
+
+    public void movingDown(boolean moving) {
+        if (moving) {
+            directionX = 0;
+            directionY = 1; // Downwards movement increases Y-coordinate
+        }
+    }
+
+    public void movingRight(boolean moving) {
+        if (moving) {
+            directionX = 1; // Rightwards movement increases X-coordinate
+            directionY = 0;
+        }
+    }
+
+    public int getDirectionX(){
+        return directionX;
+    }
+
+    public int getDirectionY() {
+        return directionY;
+    }
 }
