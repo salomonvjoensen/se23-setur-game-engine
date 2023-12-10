88939324b143ee51c6c6eb4067645c0297b377a8
Salomon Vágadal Joensen <2022.210@student.setur.fo>
2023-12-10 11:30:34 +0000

    Adds start-game-info object

---
+++ src/main/java/com/setur/se23/snake/Snake_Objects/StartGameInfo.java
@@ -0,0 +1,22 @@
+package com.setur.se23.snake.Snake_Objects;
+
+import com.setur.se23.engine.core.Core;
+import com.setur.se23.engine.core.Entity;
+import com.setur.se23.engine.render.common.Material;
+import com.setur.se23.engine.render.common.MaterialColour;
+import com.setur.se23.engine.render.common.Texture2D;
+
+public class StartGameInfo extends Entity{
+
+    public StartGameInfo() {
+        super(new Material(
+                    new Texture2D(Core.getSprite("start-game-info.png"), 651, 409),
+                    new MaterialColour(1.0f, 1.0f, 1.0f, 1.0f)), 
+              100, 
+              100,
+              0, 
+              1, 
+              1);
+    }  
+    
+}
