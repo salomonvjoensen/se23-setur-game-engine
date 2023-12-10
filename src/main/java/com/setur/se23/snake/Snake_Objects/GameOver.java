8007dbede688c67bbef10e6c4769ecf6a2bd5b4a
Salomon Vágadal Joensen <2022.210@student.setur.fo>
2023-12-10 00:21:45 +0000

    Adds GameOver entity.

---
+++ src/main/java/com/setur/se23/snake/Snake_Objects/GameOver.java
@@ -0,0 +1,22 @@
+package com.setur.se23.snake.Snake_Objects;
+
+import com.setur.se23.engine.core.Core;
+import com.setur.se23.engine.core.Entity;
+import com.setur.se23.engine.render.common.Material;
+import com.setur.se23.engine.render.common.MaterialColour;
+import com.setur.se23.engine.render.common.Texture2D;
+
+public class GameOver extends Entity{
+
+    public GameOver() {
+        super(new Material(
+                    new Texture2D(Core.getSprite("gameover.png"), 192, 42),
+                    new MaterialColour(1.0f, 1.0f, 1.0f, 1.0f)), 
+              300, 
+              384,
+              0, 
+              1, 
+              1);
+    }  
+    
+}
