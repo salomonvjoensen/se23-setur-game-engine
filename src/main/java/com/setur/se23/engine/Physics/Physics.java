b318af35a74182e7473134a755beae172b7889da
RagnarKD <2022.207@student.setur.fo>
2023-12-14 13:04:09 +0000

    Created a physics system

---
+++ src/main/java/com/setur/se23/engine/Physics/Physics.java
@@ -0,0 +1,90 @@
+package com.setur.se23.engine.Physics;
+
+public class Physics {
+
+    private double horizontalAccel;
+    private double horizontalSpeed;
+    private double minHorizontalSpeed;
+    private double maxHorizontalSpeed;
+    private double velocityX;
+    
+    private double verticalAccel;
+    private double verticalSpeed;
+    private double maxVerticalSpeed;
+    private double minVerticalSpeed;
+    private double velocityY;
+
+    public Physics(double horizontalSpeed, double verticalSpeed,
+                   double horizontalAccel, double verticalAccel,
+                   double maxHorizontalSpeed, double maxVerticalSpeed,
+                   double minHorizontalSpeed, double minVerticalSpeed) {
+
+        this.horizontalSpeed = horizontalSpeed;
+        this.horizontalAccel = horizontalAccel;
+        this.verticalSpeed = verticalSpeed;
+        this.verticalAccel = verticalAccel;
+        this.maxHorizontalSpeed = maxHorizontalSpeed;
+        this.minHorizontalSpeed = minHorizontalSpeed;
+        this.maxVerticalSpeed = maxVerticalSpeed;
+        this.minVerticalSpeed = minVerticalSpeed;
+    }
+
+    public void physicsUpdate(double deltaTime) {
+        physicsX(deltaTime);
+        physicsY(deltaTime);
+    }
+
+    private void physicsX(double deltaTime) {
+
+        horizontalSpeed *= horizontalAccel;
+
+        velocityX += horizontalSpeed * deltaTime;
+        
+        if (horizontalSpeed > maxHorizontalSpeed) {
+            horizontalSpeed = maxHorizontalSpeed;
+        }
+
+        if (horizontalSpeed < minHorizontalSpeed) {
+            horizontalSpeed = minHorizontalSpeed;
+        }
+    }
+
+    private void physicsY(double deltaTime) {
+
+        verticalSpeed *= verticalAccel;
+
+        velocityY += verticalSpeed * deltaTime;
+
+        if (verticalSpeed > maxVerticalSpeed) {
+            verticalSpeed = maxVerticalSpeed;
+        }
+
+        if (verticalSpeed < minVerticalSpeed) {
+            verticalSpeed = minVerticalSpeed;
+        }
+    }
+
+    public void setHorizontalSpeed(double horizontalSpeed) {
+        this.horizontalSpeed = horizontalSpeed;
+    }
+
+    public void setVelocityX(double velocityX) {
+        this.velocityX = velocityX;
+    }
+
+    public double getVelocityX() {
+        return velocityX;
+    }
+
+    public void setVerticalSpeed(double verticalSpeed) {
+        this.verticalSpeed = verticalSpeed;
+    }
+
+    public void setVelocityY(double velocityY) {
+        this.velocityY = velocityY;
+    }
+    
+    public double getVelocityY() {
+        return velocityY;
+    }
+}
