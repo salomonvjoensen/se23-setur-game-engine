72d44c97acdc8b4d8c1cfadb701caa5eb6bfa876
Salomon Vágadal Joensen <2022.210@student.setur.fo>
2023-12-17 16:14:50 +0000

    Renames HISS to SNAKE_HISS

--- src/main/java/com/setur/se23/snake/SoundEffects.java
+++ src/main/java/com/setur/se23/snake/SoundEffects.java
@@ -3,7 +3,7 @@ package com.setur.se23.snake;
 public enum SoundEffects {
     
     EAT_APPLE("sound-effects/apple-bite.mp3"),
-    HISS("sound-effects/snake-hiss.mp3");
+    SNAKE_HISS("sound-effects/snake-hiss.mp3");
 
     private final String filePath;
 
